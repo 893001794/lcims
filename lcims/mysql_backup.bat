f272f29b6f2a23d0afdd138f19e015733c89a158
yrg1490484943 <1490484943@qq.com>
2018-07-22 16:43:37 +0800

    更改后提交代码

--- lcims/mysql_backup.bat
+++
@@ -1,4 +0,0 @@
-net stop mysql
-xcopy "C:\Program Files\MySQL\MySQL Server 5.0\data\ims\*.*" "C:\mysql_bak\ims\%date:~0,11%\" /y
-xcopy "C:\Program Files\MySQL\MySQL Server 5.0\data\invoicemanager\*.*" "C:\mysql_bak\invoicemanager\%date:~0,11%\" /y
-net start mysql
--- lcims/src/com/lccert/oa/dao/AttendDao.java
+++
@@ -1,7 +0,0 @@
-package com.lccert.oa.dao;
-
-public interface AttendDao {
-	//获取attend表中的type自动
-	public int getType(String dateStr);
-
-}
